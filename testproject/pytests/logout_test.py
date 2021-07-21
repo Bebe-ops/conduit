f96ab99134e136ca9f9157370bfcd5b9e49621fa
Bernadett Szokol <64404343+Bebe-ops@users.noreply.github.com>
2021-07-21 15:41:22 +0200

    Update logout_test.py

--- testproject/pytests/logout_test.py
+++ testproject/pytests/logout_test.py
@@ -12,8 +12,8 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)  # headless mód
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)  # headless mód
 
 
 # A006, CON_TC09_Logout
