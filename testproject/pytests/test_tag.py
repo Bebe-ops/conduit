05540a0f541c22733a418b36fc382dcfc104dd3f
Bebe-ops <64404343+Bebe-ops@users.noreply.github.com>
2021-08-03 15:19:13 +0200

    changed names

--- testproject/pytests/test_tag.py
+++ testproject/pytests/test_tag.py
@@ -9,7 +9,7 @@ from random import randint
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.headless = False
+options.headless = True
 
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
 
